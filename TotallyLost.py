0db3baa89b0b0f21276f3723fee7e184136160a9
Christian Bongcayao <61230402+AUNTY-BETTY-pnppb@users.noreply.github.com>
2021-12-20 15:24:40 +0000

    checking if brackets work

--- TotallyLost.py
+++ TotallyLost.py
@@ -1,19 +1,20 @@
 import discord
 import os
-
+import re
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(status=discord.Status.online)
+    print("Holy shit, where are we?")
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+    if message.author.name != "Fblthp-Bot":
+        cards = re.findall("\[\[([^\[\]]*)\]\]", message.content)
+        if len(cards) > 0 :
+            for i in cards:
+                await message.channel.send(i)
+        
 my_token = os.environ['Fblthp']
 client.run(my_token)
