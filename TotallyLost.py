3e9e4badb252bdef943d1c5e7ab2e1f4ab621398
Christian Bongcayao <61230402+AUNTY-BETTY-pnppb@users.noreply.github.com>
2021-12-21 20:21:41 +0000

    moved to folder

---
+++ TotallyLost.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import re
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online)
+    print("Holy shit, where are we?")
+
+@client.event
+async def on_message(message):
+    if message.author.name != "Fblthp-Bot":
+        cards = re.findall("\[\[([^\[\]]*)\]\]", message.content)
+        if len(cards) > 0 :
+            for i in cards:
+                await message.channel.send("i")
+        
+my_token = os.environ['Fblthp']
+client.run(my_token)
