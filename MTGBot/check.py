de22e15b8f122267c1f3956aafe93bdf09f538ae
Christian Bongcayao <cbongcayao64@gmail.com>
2021-12-22 22:01:27 +0000

    improved the checks and included a search engine

--- MTGBot/check.py
+++ MTGBot/check.py
@@ -1,4 +1,3 @@
-import re
 import json
 
 # Opening JSON file
@@ -12,22 +11,36 @@ data = json.load(f)
 cardDictionary = {}
 
 # Iterating through the json
-# list
 for i in data["cards"]:
-    print(i['name'])
+    # If the card is commander legal
     if i['legalities']['commander'] == 'legal':
-        if "//" in i['name']:
-            if ('Creature' in i['type_line']) or ('Land' in i['type_line']):
-                if ('Sorcery' not in i['type_line']) and ('Instant' not in i['type_line']):
-                    cardDictionary[i['name']] = [i['card_faces'][0]['image_uris']['large'], i['card_faces'][1]['image_uris']['large']]
-                else:
-                    cardDictionary[i['name']] = [i['image_uris']['large']]
-
-            elif any(word in 'Instant Sorcery' for word in i['type_line']):
-                cardDictionary[i['name']] = [i['image_uris']['large']]
+        # check if the image is not double-faced
+        if 'image_uris' in i.keys():
+            cardDictionary[i['name']] = {'image' : [i['image_uris']['large']], 'euro' : i['prices']['eur']}
+        # else for the double-faced
         else:
-            cardDictionary[i['name']] = [i['image_uris']['large']]
-    
-print(cardDictionary[1]) 
+            cardDictionary[i['name']] = {'image' : [i['card_faces'][0]['image_uris']['large'],
+                                         i['card_faces'][1]['image_uris']['large']], 'euro' : i['prices']['eur']}
+
 # Closing file
 f.close()
+
+# search engine for cards
+def search():
+    # take input
+    print('Read to take input: ')
+    userInput = input()
+
+    # make sure it is valid length
+    if len(userInput) > 2:
+        # sift through the dictionary
+        for card in cardDictionary.keys():
+            # compare the card with the input
+            if any(word in userInput.lower().split() for word in card.lower().split()):
+                print(card, end=" ")
+                print(cardDictionary[card])
+    # else print as invalid
+    else:
+        print('Invalid input length of ' + str(len(userInput)) + ' characters')
+
+search()
